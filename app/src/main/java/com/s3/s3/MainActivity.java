817bb188ffb856f24da4107e5b0af719458346db
Amitav Khandelwal <amitav.khandelwal@gmail.com>
2016-11-12 10:46:19 +0530

    AppIntro setup, first 2 slides work

--- app/src/main/java/com/s3/s3/MainActivity.java
+++ app/src/main/java/com/s3/s3/MainActivity.java
@@ -1,13 +1,63 @@
 package com.s3.s3;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.github.paolorotolo.appintro.AppIntro2;
+import com.github.paolorotolo.appintro.AppIntroFragment;
+
+public class MainActivity extends AppIntro2 {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        addSlide(AppIntroFragment.newInstance(getString(R.string.intro_title), getString(R.string.intro_description),
+                R.drawable.ic_brightness_7_black_24dp, getResources().getColor(R.color.colorPrimary)
+                //,getResources().getColor(R.color.text_black), getResources().getColor(R.color.text_black)
+        ));
+        addSlide(new MonthlyBillFragment());
+        addSlide(new RooftopAreaFragment());
+        showSkipButton(false);
+        setCustomTransformer(new ZoomOutPageTransformer());
+    }
+
+    public class ZoomOutPageTransformer implements ViewPager.PageTransformer {
+        private static final float MIN_SCALE = 0.85f;
+        private static final float MIN_ALPHA = 0.5f;
+
+        public void transformPage(View view, float position) {
+            int pageWidth = view.getWidth();
+            int pageHeight = view.getHeight();
+
+            if (position < -1) { // [-Infinity,-1)
+                // This page is way off-screen to the left.
+                view.setAlpha(0);
+
+            } else if (position <= 1) { // [-1,1]
+                // Modify the default slide transition to shrink the page as well
+                float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
+                float vertMargin = pageHeight * (1 - scaleFactor) / 2;
+                float horzMargin = pageWidth * (1 - scaleFactor) / 2;
+                if (position < 0) {
+                    view.setTranslationX(horzMargin - vertMargin / 2);
+                } else {
+                    view.setTranslationX(-horzMargin + vertMargin / 2);
+                }
+
+                // Scale the page down (between MIN_SCALE and 1)
+                view.setScaleX(scaleFactor);
+                view.setScaleY(scaleFactor);
+
+                // Fade the page relative to its size.
+                view.setAlpha(MIN_ALPHA +
+                        (scaleFactor - MIN_SCALE) /
+                                (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+
+            } else { // (1,+Infinity]
+                // This page is way off-screen to the right.
+                view.setAlpha(0);
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/s3/s3/RooftopAreaFragment.java
@@ -0,0 +1,29 @@
+package com.s3.s3;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RooftopAreaFragment extends Fragment {
+
+
+    public RooftopAreaFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_rooftop_area, container, false);
+    }
+
+}
